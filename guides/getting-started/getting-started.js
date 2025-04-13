b72588a702506b3c03a336d7dd5baaa568540dff
Mubarak Harran Alketbi <mubarak.harran@gmail.com>
2025-04-13 12:35:58 +0400

    fourth commit

---
+++ guides/getting-started/getting-started.js
@@ -0,0 +1,3 @@
+// No specific JavaScript functionality required for guides/getting-started/index.html
+// It relies on the global /script.js for footer year, etc.
+console.log("Getting Started guide index page loaded.");
---
+++ guides/guides.js
@@ -0,0 +1,3 @@
+// No specific JavaScript functionality required for guides/index.html
+// It relies on the global /script.js for footer year, etc.
+console.log("Guides index page loaded.");
